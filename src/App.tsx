e34cfaab5de04434b0573404d70d0ca9d79a6977
junkor-1011 <tomoyukizxy@gmail.com>
2022-09-06 01:18:55 +0900

    enable scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import './App.css';
+import './App.scss';
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
