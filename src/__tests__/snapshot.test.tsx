ed1774a7e8f20573e9889df5ab1779f5f6fd8362
junkor-1011 <tomoyukizxy@gmail.com>
2022-09-07 01:30:58 +0900

    improve snapshot test by @emotion/jest

--- src/__tests__/snapshot.test.tsx
+++ src/__tests__/snapshot.test.tsx
@@ -1,5 +1,8 @@
 import { render } from '@testing-library/react';
+import { createSerializer } from '@emotion/jest';
 import App from '../App';
+
+expect.addSnapshotSerializer(createSerializer());
 
 describe('snapshot test', () => {
   it('App Component', () => {
